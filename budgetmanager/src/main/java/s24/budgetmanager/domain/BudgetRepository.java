51b985c0f34faa7cc0cad2da49e89ad8d4c8df3e
Alisa <Alisa@MacBook-Pro.N4>
2024-04-24 14:42:21 +0300

    budget history added

--- budgetmanager/src/main/java/s24/budgetmanager/domain/BudgetRepository.java
+++ budgetmanager/src/main/java/s24/budgetmanager/domain/BudgetRepository.java
@@ -1,9 +1,13 @@
 package s24.budgetmanager.domain;
 
 
+
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface BudgetRepository extends CrudRepository<Budget, Long> {
     Budget findByName(String name);
     Budget findFirstByOrderByBudgetidDesc();
+    List<Budget> findAll();
 }
--- budgetmanager/src/main/java/s24/budgetmanager/web/BudgetController.java
+++ budgetmanager/src/main/java/s24/budgetmanager/web/BudgetController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -158,4 +159,11 @@ public class BudgetController {
         return "redirect:/purchaselist"; 
     }
 
-}
+
+    @GetMapping("/budgethistory")
+    public String showBudgetHistory(Model model) {
+        model.addAttribute("budgets", brepository.findAll());
+        return "budgethistory";
+    }
+
+}   
